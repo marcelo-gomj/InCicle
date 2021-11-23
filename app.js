2a44f4e74048a22daae6308a402cdb8dcdfe0cc8
Marcelo <marcelomgj005@gmail.com>
2021-11-23 16:56:02 -0300

    add acessibility

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ const boxPost = query('.posts');
 // get the informations of /data.json
 async function getPosts(){
     const data = await fetch('/data.json');
-    if(data.ok) return data.json();
+    if(data.ok){
+        return data.json();
+    }else{
+        alert('Erro ao encontrar os dados')
+    }
 }
 
 // create posts card
 async function addMainPosts(){
     const posts = await getPosts();
-    posts.data.map(item => createCard(item));
+    posts.data.map((item, index) => createCard(item, index));
 }
 
 function getTypePost(type) {
@@ -30,20 +34,22 @@ function getMoreDetails(info) {
     const convited = info.invited_people
     if(convited){
         const confirmed = convited.filter(people => people.confirmed);
-        return `${confirmed.length} CONFIRMACÕES DE ${convited.length}`;
+        return `${confirmed.length} CONFIRMAÇÕES DE ${convited.length}`;
     }else{
         return '';
     }
 }
 
 // Add elements in DOM
-function createCard(item){
-    const div = document.createElement('div');
+function createCard(item, setindex) {
+    const div = document.createElement('article')
+        , title = item.title
+        , postType = item.type;
     const elements = [
-        `<img src="${item.files[0].file}">`,
-        `<div><h3>${item.title}</h3>`,
+        `<img src="${item.files[0].file}" alt="${title}">`,
+        `<div><h3>${title}</h3>`,
         `<div>
-            <span class="${item.type}">${getTypePost(item.type)}</span>
+            <span class="${postType}">${getTypePost(postType)}</span>
             <span>${item.date}</span>
             <span class="post-confirms">${getMoreDetails(item.common)}</span>
         </div>`,
@@ -51,15 +57,19 @@ function createCard(item){
     ];
 
     div.innerHTML = elements.join('');
+    div.tabIndex = setindex + 1;
+    div.setAttribute('role', 'article')
     boxPost.appendChild(div);
 }
 
 addMainPosts();
 
-// Add btns actions
+// Add buttons actions
 const btnAbout = query('.bar-about button');
 function hideAbout(e){
-    e.target.parentElement.style.display = 'none';
+    const boxAbout = e.target.parentElement;
+    boxAbout.style.display = 'none';
+    boxAbout.style.ariaHidden = 'false';
 }
 
 btnAbout.addEventListener('click', hideAbout)
@@ -75,6 +85,3 @@ const list = query('.type-list');
         list.classList.remove('hidden-list')
     })
 }())
-
-
-
